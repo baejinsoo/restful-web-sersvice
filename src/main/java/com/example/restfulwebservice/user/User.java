c80e5e73c63483221ed7397211babf6df30169f5
baejinsoo <jinsoo941010@naver.com>
2021-04-13 21:34:49 +0900

    Client 에게 중요정보  전달해주지 않기위한@JsonIgnore 애너테이션

--- src/main/java/com/example/restfulwebservice/user/User.java
+++ src/main/java/com/example/restfulwebservice/user/User.java
@@ -1,5 +1,7 @@
 package com.example.restfulwebservice.user;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -9,6 +11,7 @@ import java.util.Date;
 
 @Data
 @AllArgsConstructor
+@JsonIgnoreProperties(value = {"password","ssn"}) // 한번에 여러개의 중요정보 Client 에게 전달해주지 않기위한 애너테이션
 public class User {
     private Integer id;
 
@@ -16,4 +19,8 @@ public class User {
     private String name;
     @Past
     private Date joinDate;
+
+    // @JsonIgnore // 중요정보 Client 에게 전달해주지 않기위한 애너테이션
+    private String password;
+    private String ssn;
 }
--- src/main/java/com/example/restfulwebservice/user/UserDaoService.java
+++ src/main/java/com/example/restfulwebservice/user/UserDaoService.java
@@ -15,9 +15,9 @@ public class UserDaoService { // 비즈니스 로직과 관련된 부분은 Serv
     private static int userCount = 3;
 
     static {
-        users.add(new User(1, "jinsoo1", new Date()));
-        users.add(new User(2, "jinsoo2", new Date()));
-        users.add(new User(3, "jinsoo3", new Date()));
+        users.add(new User(1, "jinsoo1", new Date(), "pass1", "941010-1111111"));
+        users.add(new User(2, "jinsoo2", new Date(),"pass1", "941010-1111111"));
+        users.add(new User(3, "jinsoo3", new Date(),"pass1", "941010-1111111"));
     }
 
     public List<User> findAll() {
